690f3a067a4002186b811b66806d398bb25bc11a
Batraisk <batraisk@gmail.com>
2021-09-26 02:08:46 +0300

    add test message

--- app/services/facebook/pixel.rb
+++ app/services/facebook/pixel.rb
@@ -10,55 +10,61 @@ module Facebook
 
     end
 
-    def call
-      return unless @access_token and @pixel_id
-      FacebookAds.configure do |config|
-        config.access_token = access_token
-      end
-
-      user_data = FacebookAds::ServerSide::UserData.new(
-        client_ip_address: guest.remote_ip,
-        client_user_agent: guest.user_agent,
-      )
-
-      event = FacebookAds::ServerSide::Event.new(
-        event_name: 'Instagram subscription',
-        event_time: Time.now.to_i,
-        user_data: user_data,
-        action_source: 'website'
-      )
-
-
-      request = FacebookAds::ServerSide::EventRequest.new(
-        pixel_id: pixel_id,
-        events: [event],
-        test_event_code: "TEST77185"
-      )
-      request.execute
-    end
     # def call
-    #   access_token = @access_token
-    #   pixel_id = @pixel_id
-    #
+    #   return unless @access_token and @pixel_id
     #   FacebookAds.configure do |config|
     #     config.access_token = access_token
     #   end
     #
-    #   user_data_0 = FacebookAds::ServerSide::UserData.new(
-    #     client_ip_address: "::1",
-    #     client_user_agent: "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/93.0.4577.82 Safari\/537.36")
-    #   event_0 = FacebookAds::ServerSide::Event.new(
-    #     event_name: "Instagram subscription test",
-    #     event_time: 1632608007,
-    #     user_data: user_data_0,
-    #     # custom_data: custom_data_0,
-    #     action_source: "website")
+    #   user_data = FacebookAds::ServerSide::UserData.new(
+    #     client_ip_address: guest.remote_ip,
+    #     client_user_agent: guest.user_agent,
+    #   )
+    #
+    #   event = FacebookAds::ServerSide::Event.new(
+    #     event_name: 'Instagram subscription test',
+    #     event_time: Time.now.to_i,
+    #     user_data: user_data,
+    #     action_source: 'website'
+    #   )
+    #
     #
     #   request = FacebookAds::ServerSide::EventRequest.new(
     #     pixel_id: pixel_id,
-    #     events: [event_0])
+    #     events: [event],
+    #   )
     #   request.execute
     # end
+    def call
+      access_token = @access_token
+      pixel_id = @pixel_id
+
+      FacebookAds.configure do |config|
+        config.access_token = access_token
+      end
+
+      user_data_0 = FacebookAds::ServerSide::UserData.new(
+        client_ip_address: "::1",
+        client_user_agent: "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/93.0.4577.82 Safari\/537.36")
+
+
+      # event_0 = FacebookAds::ServerSide::Event.new(
+      #   event_name: "Instagram subscription test",
+      #   event_time: 1632608007,
+      #   user_data: user_data_0,
+      #   action_source: "website")
+
+
+      request = FacebookAds::ServerSide::EventRequest.new(
+        pixel_id: pixel_id,
+        events: [FacebookAds::ServerSide::Event.new(
+          event_name: "Instagram subscription test",
+          event_time: 1632608007,
+          user_data: user_data_0,
+          action_source: "website")],
+        test_event_code: "TEST77185")
+        request.execute
+    end
 
 
   end
