310efcdc8a779242c53ae2ac82d767056764b4dd
Batraisk <batraisk@gmail.com>
2021-10-06 15:36:37 +0300

    bugfixes

--- app/models/admin_authorization.rb
+++ app/models/admin_authorization.rb
@@ -2,8 +2,8 @@ class AdminAuthorization < ActiveAdmin::AuthorizationAdapter
 
   def authorized?(action, subject = nil)
     resources = ActiveAdmin::ManageableResource.call.map {|t| t[:class_name]}
-    if resources.include?(subject.class.to_s)
-      user.available_actions(subject.class.to_s).include?(action.to_s)
+    if resources.include?(subject.class.to_s) or resources.include?(subject.to_s)
+      user.available_actions(subject.class.to_s).include?(action.to_s) or user.available_actions(subject.to_s).include?(action.to_s)
     else
       true
     end
